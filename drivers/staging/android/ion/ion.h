22cb13e27a755d4c54c1b111b4838ebb8c330bb4
Ashish Kalra <eashishkalra@gmail.com>
2017-12-02 22:59:27 +0530

    staging: android: add identifiers to function parameters
    Fix checkpatch.pl warnings of the form "function definition argument
    'foo' should also have an identifier name".
    
    Signed-off-by: Ashish Kalra <eashishkalra@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/ion/ion.h
+++ drivers/staging/android/ion/ion.h
@@ -257,7 +257,8 @@ struct ion_heap {
 
 	atomic_long_t total_allocated;
 
-	int (*debug_show)(struct ion_heap *heap, struct seq_file *, void *);
+	int (*debug_show)(struct ion_heap *heap, struct seq_file *s,
+			  void *unused);
 };
 
 /**
